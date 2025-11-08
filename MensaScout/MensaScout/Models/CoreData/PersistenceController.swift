0239a64cfdca0dc3d828ffdebe612aebc9b132b6
official-Cromatin <lars-winzer@web.de>
2025-11-08 17:49:08 +0100

    Add: PersistenceController for Core Data

---
+++ MensaScout/MensaScout/Models/CoreData/PersistenceController.swift
@@ -0,0 +1,31 @@
+//
+//  PersistenceController.swift
+//  MensaScout
+//
+//  Created by Lars Winzer on 08.11.25.
+//
+
+import CoreData
+
+struct PersistenceController {
+    static let shared = PersistenceController()
+
+    let container: NSPersistentContainer
+
+    init(inMemory: Bool = false) {
+        container = NSPersistentContainer(name: "MensaScoutModel")
+
+        if inMemory {
+            // Speichert nur im RAM, nicht auf der Festplatte
+            container.persistentStoreDescriptions.first!.url = URL(fileURLWithPath: "/dev/null")
+        }
+
+        container.loadPersistentStores { description, error in
+            if let error = error as NSError? {
+                fatalError("Unresolved Core Data error: \(error), \(error.userInfo)")
+            }
+        }
+
+        container.viewContext.automaticallyMergesChangesFromParent = true
+    }
+}
